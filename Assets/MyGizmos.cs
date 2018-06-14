fb376c7c92f05f7570424063c60721f385d0c669
cheitovilla <>
2018-06-13 20:45:26 -0500

    aquiesta

---
+++ Assets/MyGizmos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour {
+
+
+    void OnDrawGizmos()
+    {
+     //   Gizmos.DrawCube(new Vector3(0, 0, 0), new Vector3(1, 2, 3));
+      //  Gizmos.DrawRay(new Ray(new Vector3(0, 0, 0), new Vector3(0, 1, 0)));
+        Ray r = new Ray();
+        r.origin = new Vector3(0, 0, 0);
+        r.direction = new Vector3(0, 1, 0);
+        Gizmos.DrawRay(r);
+        //Gizmos.DrawLine(transform.position, new Vector3(0, 0, 0));
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
